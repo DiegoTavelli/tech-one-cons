7f59862ebc1b6c15e750e2466313008bf4594ee2
DiegoTavelli <diegotavelli@gmail.com>
2022-11-29 04:47:07 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 8080 //3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
