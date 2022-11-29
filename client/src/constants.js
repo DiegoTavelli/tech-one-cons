8239a109f7cc94644cb24586794a9d1ddeade532
DiegoTavelli <diegotavelli@gmail.com>
2022-11-29 04:50:30 -0300

    deploy2

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-export const BASE_URL = 'tech-one-cons.fly.dev';
+export const BASE_URL = 'https://tech-one-cons.fly.dev';
 export const POST_PROFILE_URL = '/profile';
 export const GET_USERS_URL = '/profile';
 
 // export const BASE_URL = 'http://localhost:3001';
-// export const BASE_URL = 'tech-one-cons.fly.dev';
+// export const BASE_URL = 'https://tech-one-cons.fly.dev';
