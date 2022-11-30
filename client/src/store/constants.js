7c8842a774bdc1d4f24369ba086e585d86fe9aef
DiegoTavelli <diegotavelli@gmail.com>
2022-11-29 21:54:48 -0300

    css

--- client/src/store/constants.js
+++ client/src/store/constants.js
@@ -3,3 +3,4 @@ export const SET_ACADEMICS = 'SET_ACADEMICS';
 export const POST_PERSON = 'POST_PERSON';
 export const CLEAR_POST = 'CLEAR_POST';
 export const GET_PERSONS = 'GET_PERSONS';
+export const CLEAR_STATES = 'CLEAR_STATES';
